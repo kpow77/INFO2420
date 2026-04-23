8b7a7f824f9918059f5161bc04cc1db98bdc2f1f
KylerPowell <10736335@uvu.edu>
2026-04-23 00:27:23 -0600

    o

--- project/project7/Myscripts.js
+++ project/project7/Myscripts.js
@@ -1,56 +1,42 @@
-// JavaScript Document
 
-PrintButton1 = new Image
-	PrintButton2 = new Image
-	logo1 = new Image
-	logo2 = new Image
-if (document.images) {
-	PrintButton1.src = "images/print5.gif"
-	PrintButton2.src = "images/print6.gif"
-	logo1.src = "images/logo1.jpg"
-	logo2.src = "images/logo2.jpg"
-}
+    var PrintButton1 = new Image();
+    var PrintButton2 = new Image();
+    var logo1 = new Image();
+    var logo2 = new Image();
 
+    if (document.images) {
+        PrintButton1.src = "images/print5.gif";
+        PrintButton2.src = "images/print6.gif";
+        logo1.src = "images/logo1.jpg";
+        logo2.src = "images/logo2.jpg";
+    }
 
-		var imgArray = new Array(
-			'Image1Lg.jpg',
-			'Image2Lg.jpg',
-			'Image3Lg.jpg',
-			'Image4Lg.jpg'
-			
+    var imgArray = [
+        "Image1Lg.jpg",
+        "Image2Lg.jpg",
+        "Image3Lg.jpg",
+        "Image4Lg.jpg"
+    ];
 
-		);
+    var titleArray = [
+        "Bald Eagle",
+        "Group of Geese",
+        "Heron",
+        "2 Pelicans"
+    ];
 
+    var imgPath = "images/";
 
-		var titleArray = new Array(
-			'Dog Performing a Trick',
-			'Two Dogs',
-			'Dog and Two Boys Portrait',
-			'Woman and Dog'
-			
-		);
-			
-		var imgPath = "Images/Fullsize/";
-		
-		function swapImage(imgID) {
+    function swapImage(imgID) {
+        document.getElementById("theImage").src = imgPath + imgArray[imgID];
+        document.getElementById("bottomText").innerHTML = titleArray[imgID];
+    }
 
-			var theImage = document.getElementById('theImage');
-			var textDiv = document.getElementById('bottomText');
+    function preloadImages() {
+        for (var i = 0; i < imgArray.length; i++) {
+            var tmpImg = new Image();
+            tmpImg.src = imgPath + imgArray[i];
+        }
+    }
 
-			var newImg;
-			var textTitle;
-
-			newImg = imgArray[imgID];
-			theImage.src = imgPath + newImg;
-
-			textTitle=titleArray[imgID];
-
-      			textDiv.innerHTML = textTitle;
-		}
-			
-		function preloadImages() {		
-			for(var i = 0; i < imgArray.length; i++) {
-				var tmpImg = new Image;
-				tmpImg.src = imgPath + imgArray[i];
-			}
-		}
+    window.onload = preloadImages;
