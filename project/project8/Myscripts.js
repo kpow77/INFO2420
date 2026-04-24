d85a7d435349f3ea49d9c3c98058edcc416fb34e
KylerPowell <10736335@uvu.edu>
2026-04-23 23:22:41 -0600

    s

--- project/project8/Myscripts.js
+++ project/project8/Myscripts.js
@@ -5,39 +5,38 @@
     var logo2 = new Image();
 
     if (document.images) {
-        PrintButton1.src = "images/print1.jpg";
-        PrintButton2.src = "images/print2.jpg";
+        PrintButton1.src = "images/print5.gif";
+        PrintButton2.src = "images/print6.gif";
         logo1.src = "images/logo1.jpg";
         logo2.src = "images/logo2.jpg";
     }
 
- var imgArray = [
-    "Image1Lg.jpg",
-    "Image2Lg.jpg",
-    "Image3Lg.jpg",
-    "Image4Lg.jpg"
-];
+    var imgArray = [
+        "Image1Lg.jpg",
+        "Image2Lg.jpg",
+        "Image3Lg.jpg",
+        "Image4Lg.jpg"
+    ];
 
-var titleArray = [
-    "Bald Eagle",
-    "Group of Geese",
-    "Heron",
-    "2 Pelicans"
-];
+    var titleArray = [
+        "Bald Eagle",
+        "Group of Geese",
+        "Heron",
+        "2 Pelicans"
+    ];
 
-var imgPath = "images/";
+    var imgPath = "images/";
 
-function swapImage(imgID) {
-    document.getElementById("theImage").src = imgPath + imgArray[imgID];
-    document.getElementById("bottomText").innerHTML = titleArray[imgID];
-}
-
-function preloadImages() {
-    for (var i = 0; i < imgArray.length; i++) {
-        var tmpImg = new Image();
-        tmpImg.src = imgPath + imgArray[i];
+    function swapImage(imgID) {
+        document.getElementById("theImage").src = imgPath + imgArray[imgID];
+        document.getElementById("bottomText").innerHTML = titleArray[imgID];
     }
-}
+
+    function preloadImages() {
+        for (var i = 0; i < imgArray.length; i++) {
+            var tmpImg = new Image();
+            tmpImg.src = imgPath + imgArray[i];
+        }
+    }
 
     window.onload = preloadImages;
-
